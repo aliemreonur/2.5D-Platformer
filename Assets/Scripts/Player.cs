939d4f061fa693f6060680cd30a36bf80a948ba8
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-04-24 23:04:54 +0300

    Character Controller and Double Jump Completed - Checked OK

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    [SerializeField] private float _speed = 5f;
+    private float _horizontalInput;
+    [SerializeField] private float _gravity = 1.3f;
+    [SerializeField] private float _jumpHeight = 25f;
+    private float _yVelocity;
+    private bool _canDooubleJump;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();        
+    }
+
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(_horizontalInput, 0, 0) ;
+        Vector3 velocity = direction * _speed;
+        
+        if(_characterController.isGrounded)
+        { 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDooubleJump = true;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_canDooubleJump)
+                {
+                    _yVelocity += _jumpHeight *1.5f;
+                    _canDooubleJump = false;
+                }
+            }
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        _characterController.Move(velocity * Time.deltaTime);
+
+        //Physics.Raycast(transform.position, Vector3.down * 2f, 2 << 8);
+        //RaycastHit hitInfo;
+
+    }
+}

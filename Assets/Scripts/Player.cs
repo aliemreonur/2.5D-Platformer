e9257f3a26fb140f8500ce34306f60facacf8022
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-04-26 19:31:34 +0300

    Added Lives System w GameOver - Checked OK

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,21 +12,30 @@ public class Player : MonoBehaviour
     private float _yVelocity;
     private bool _canDooubleJump;
 
-    private int coins;
+    Vector3 _spawnPoint;
+
+    private int _coins;
+    private int _lives = 3;
 
     void Start()
     {
-        _characterController = GetComponent<CharacterController>();        
+        _characterController = GetComponent<CharacterController>();
+        _spawnPoint = new Vector3(-7f, 2.43f, 0);
     }
 
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         _horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(_horizontalInput, 0, 0) ;
+        Vector3 direction = new Vector3(_horizontalInput, 0, 0);
         Vector3 velocity = direction * _speed;
-        
-        if(_characterController.isGrounded)
-        { 
+
+        if (_characterController.isGrounded)
+        {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
@@ -35,9 +44,9 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if(_canDooubleJump)
+                if (_canDooubleJump)
                 {
                     _yVelocity += _jumpHeight;
                     _canDooubleJump = false;
@@ -48,17 +57,47 @@ public class Player : MonoBehaviour
 
         velocity.y = _yVelocity;
         _characterController.Move(velocity * Time.deltaTime);
+    }
 
-        if(transform.position.y < -5)
-        {
-            Debug.Log("Game Over");
-        }
+    private void Death()
+    {
 
     }
 
     public void CoinCollected()
     {
-        coins++;
-        UIManager.Instance.UpdateScore(coins);
+        _coins++;
+        UIManager.Instance.UpdateScore(_coins);
     }
+
+    IEnumerator DeathRoutine()
+    {
+        for(int i = 0; i<2; i++)
+        {
+            transform.position = new Vector3(-7.5f, 2.4f, 0);
+            _lives--;
+            UIManager.Instance.UpdateLives(_lives);
+            yield return new WaitForSeconds(0.5f);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "DeathZone")
+        {
+            _characterController.enabled = false;
+            _lives--;
+            UIManager.Instance.UpdateLives(_lives);
+            transform.position = _spawnPoint;
+            _characterController.enabled = true;
+            if (_lives <= 0)
+            {
+                UIManager.Instance.GameOver(_coins);
+                Debug.Log("Game Over");
+            }
+            
+        }
+    }
+
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager _instance;
-    [SerializeField] Text _scoreText;
+    [SerializeField] Text _scoreText, _livesText, _endScoreText, _gameOverText;
+    [SerializeField] GameObject gameOverScene;
+    public bool isGameOver;
 
     public static UIManager Instance
     {
@@ -25,8 +28,51 @@ public class UIManager : MonoBehaviour
         _instance = this;
     }
 
+    private void Start()
+    {
+        gameOverScene.gameObject.SetActive(false);
+    }
+
     public void UpdateScore(int score)
     {
         _scoreText.text = score.ToString();
+    }
+
+    public void UpdateLives(int lives)
+    {
+        _livesText.text = "Lives : " + lives.ToString();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        isGameOver = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void GameOver(int coins)
+    {
+        gameOverScene.SetActive(true);
+        _endScoreText.text = "You have Collected : " + coins + " Coins";
+        isGameOver = true;
+        StartCoroutine(GameOverRoutine());
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        while(isGameOver)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            _endScoreText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            _gameOverText.gameObject.SetActive(false);
+            _endScoreText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+
+        }
     }
 }

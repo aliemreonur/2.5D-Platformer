fc4d7b6f18ec5899374f6996d07752bee207540f
aliemreonur <58938983+aliemreonur@users.noreply.github.com>
2021-04-27 18:57:53 +0300

    Part2 - Elevator Movement Completed

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] private Transform _floor0, _floor1;
+    private float _speed = 5f;
+
+    public bool elevatorCalledDown;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(elevatorCalledDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _floor0.position, _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _floor1.position, _speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/ElevatorPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    Material _callButtonMat;
+    Player _player;
+    Elevator elevator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _callButtonMat = transform.GetChild(0).gameObject.GetComponent<Renderer>().material;
+        _player = FindObjectOfType<Player>().GetComponent<Player>();
+        elevator = FindObjectOfType<Elevator>().GetComponent<Elevator>();
+        if(_player == null)
+        {
+            Debug.LogError("elevator panel could not reach to the player");
+        }
+            if(_callButtonMat == null)
+        {
+            Debug.Log("Child object's material could not be reached");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E) && _player.Coins >= 8)
+            {
+                if(!elevator.elevatorCalledDown)
+                {
+                    _callButtonMat.color = Color.green;
+                    elevator.elevatorCalledDown = true;
+                    Debug.Log("Elevator going down");
+                }
+                else
+                {
+                    elevator.elevatorCalledDown = false;
+                    Debug.Log("Elevator going up");
+                }
+            }
+        }
+    }
+}
